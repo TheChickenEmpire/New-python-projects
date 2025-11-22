6d0dbcc73f5a70f5f1e56bb79f8c8088c3b3ac65
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-22 21:43:18 +1300

    hu

--- apptest.py
+++ apptest.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["Gemini_api"])
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
-if "chat" not in st.session_state.chat:
+if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
 if "entire_chat" not in st.session_state:
     st.session_state.entire_chat = ""
