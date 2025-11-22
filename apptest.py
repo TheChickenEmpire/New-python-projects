18e531263acc7535cb90ef8d12c084ccba6fff3c
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-22 21:42:07 +1300

    all

--- apptest.py
+++ apptest.py
@@ -3,17 +3,14 @@ import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["Gemini_api"])
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
-try:
-    if st.session_state.times:
-        chat = model.start_chat(history=[])
-except:
-    st.session_state.times=0
+if "chat" not in st.session_state.chat:
+    st.session_state.chat = model.start_chat(history=[])
 if "entire_chat" not in st.session_state:
     st.session_state.entire_chat = ""
 message = st.chat_input("You:")
 if message:
     try:
-        resp = chat.send_message(message)
+        resp = st.session_state.chat.send_message(message)
         text = getattr(resp, "text", str(resp))
     except Exception as e:
         text = f"(error: {e})"
