e2630d5cf60ef5b057b8921a69e5c39abb7c5e64
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-22 19:33:54 +1300

    hi

--- apptest.py
+++ apptest.py
@@ -4,14 +4,13 @@ genai.configure(api_key=st.secrets["Gemini_api"])
 model=genai.GenerativeModel("gemini-2.5-flash-lite")
 chat=model.start_chat(history=[])
 entire_chat=""
-while True:
-    try:
-        message=st.chat_input("You:\n")
-        entire_chat=entire_chat+"You:\n"+message+"\n"
-        response=chat.send_message(message)
-        response=response.text
-        response="Gemini:\n"+response+'\n_________________________________________\n'
-        entire_chat=entire_chat+response
-        st.text(entire_chat)
-    except:
-        pass
+try:
+    message=st.chat_input("You:\n")
+    entire_chat=entire_chat+"You:\n"+message+"\n"
+    response=chat.send_message(message)
+    response=response.text
+    response="Gemini:\n"+response+'\n_________________________________________\n'
+    entire_chat=entire_chat+response
+    st.text(entire_chat)
+except:
+    pass
