cdb0489e5d64e9a1ac4dc145e535e3156ce3882c
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-24 09:18:50 +1300

    hu

--- apptest.py
+++ apptest.py
@@ -31,24 +31,3 @@ if message:
 
     st.session_state.entire_chat += f"You:\n{message}\nGemini:\n{text}\n_________________\n"
 st.text(st.session_state.entire_chat)
-'''
-import streamlit as st
-import google.generativeai as genai
-
-genai.configure(api_key=st.secrets["Gemini_api"])
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
-if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history=[])
-if "entire_chat" not in st.session_state:
-    st.session_state.entire_chat = ""
-message = st.chat_input("You:")
-if message:
-    try:
-        resp = st.session_state.chat.send_message(message)
-        text = getattr(resp, "text", str(resp))
-    except Exception as e:
-        text = f"(error: {e})"
-
-    st.session_state.entire_chat += f"You:\n{message}\nGemini:\n{text}\n_________________\n"
-st.text(st.session_state.entire_chat)
-'''
