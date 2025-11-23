753772bbaba9121a89386b57c8f148ada9904dc8
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-24 09:22:51 +1300

    ij

--- apptest.py
+++ apptest.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
-import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+
 genai.configure(api_key=st.secrets["Gemini_api"])
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
-st.session_state.api = 0
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
 if "entire_chat" not in st.session_state:
@@ -16,18 +13,7 @@ if message:
         resp = st.session_state.chat.send_message(message)
         text = getattr(resp, "text", str(resp))
     except Exception as e:
-        if "quota" in e:
-            if st.session_state.api == 0:
-                genai.configure(api_key=st.secrets["Gemini_api2"])
-                logger.info("Switched 2 second api key")
-                st.session_state.api = 1
-            else:
-                genai.configure(api_key=st.secrets["Gemini_api"])
-                st.session_state.api = 0
-            resp = st.session_state.chat.send_message(message)
-            text = getattr(resp, "text", str(resp))
-        else:
-            text = f"(error: {e})"
+        text = f"(error: {e})"
 
     st.session_state.entire_chat += f"You:\n{message}\nGemini:\n{text}\n_________________\n"
 st.text(st.session_state.entire_chat)
