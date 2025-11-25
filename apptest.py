73e8b0cbd9f87b6615c788353be46e33378d8182
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-26 09:35:03 +1300

    wec

--- apptest.py
+++ apptest.py
@@ -16,7 +16,6 @@ with st.sidebar:
         st.session_state.entire_chat += "New chat:\n__________________________________\n"
 if message:
     try:
-        st.session_state.entire_chat += f"You:\n{message}\n"
         resp = st.session_state.chat.send_message(message)
         text = getattr(resp, "text", str(resp))
     except Exception as e:
