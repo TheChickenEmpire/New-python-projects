6173c48e57b88af3b8116a01315028e3a4074f54
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-26 11:43:29 +1300

    chicihe

--- apptest.py
+++ apptest.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 genai.configure(api_key=st.secrets["Gemini_api"])
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
+st.session_state.apinum = 0
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
 if "entire_chat" not in st.session_state:
@@ -19,7 +20,17 @@ if message:
         resp = st.session_state.chat.send_message(message)
         text = getattr(resp, "text", str(resp))
     except Exception as e:
-        text = f"(error: {e})"
-        logger.error(text)
+        if "quota" in e:
+            if st.session_state.apinum == 0:
+                genai.configure(api_key=st.secrets["Gemini_api2"])
+                st.session_state.apinum = 1
+                logger.info("Switched to api 2")
+            elif st.session_state.apinum == 1:
+                genai.configure(api_key=st.secrets["Gemini_api"])
+                st.session_state.apinum = 1
+                logger.info("Switched to api 1")
+            else:
+                text = f"(error: {e})"
+                logger.error(text)
     st.session_state.entire_chat += f"You:\n{message}\nGemini:\n{text}\n__________________________________\n"
 st.text(st.session_state.entire_chat)
