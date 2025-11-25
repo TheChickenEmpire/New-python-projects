b256dc8728aa9a35ae7526ee09d2a2d2ce5188df
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-26 11:51:08 +1300

    knj

--- apptest.py
+++ apptest.py
@@ -5,7 +5,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 genai.configure(api_key=st.secrets["Gemini_api"])
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
-st.session_state.apinum = 0
+if "apinum" not in st.session_state:
+    st.session_state.apinum = 0
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
 if "entire_chat" not in st.session_state:
@@ -20,7 +21,7 @@ if message:
         resp = st.session_state.chat.send_message(message)
         text = getattr(resp, "text", str(resp))
     except Exception as e:
-        if "quota" in e:
+        if "quota" in str(e).lower():
             if st.session_state.apinum == 0:
                 genai.configure(api_key=st.secrets["Gemini_api2"])
                 st.session_state.apinum = 1
