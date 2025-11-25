ff8353056a83a395c4b521ab2933750a4d73994f
Lucas Zhao <thenewchickenempire@gmail.com>
2025-11-26 09:00:13 +1300

    4f3rf43

--- apptest.py
+++ apptest.py
@@ -10,9 +10,10 @@ if "chat" not in st.session_state:
 if "entire_chat" not in st.session_state:
     st.session_state.entire_chat = ""
 message = st.chat_input("You:") 
-if st.button("RESET"):
-    st.session_state.chat = model.start_chat(history=[])
-    st.session_state.entire_chat += "New chat:\n--------------------"
+with st.sidebar:
+    if st.button("RESET"):
+        st.session_state.chat = model.start_chat(history=[])
+        st.session_state.entire_chat += "New chat:\n--------------------"
 if message:
     try:
         resp = st.session_state.chat.send_message(message)
